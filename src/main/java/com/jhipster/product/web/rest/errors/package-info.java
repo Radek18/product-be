cc8290968ede4e9207d602f54e6a771244314363
Radek <>
2024-09-25 10:18:41 +0200

    Initial commit

---
+++ src/main/java/com/jhipster/product/web/rest/errors/package-info.java
@@ -0,0 +1,4 @@
+/**
+ * Rest layer error handling.
+ */
+package com.jhipster.product.web.rest.errors;
---
+++ src/test/java/com/jhipster/product/IntegrationTest.java
@@ -0,0 +1,27 @@
+package com.jhipster.product;
+
+import com.jhipster.product.config.AsyncSyncConfiguration;
+import com.jhipster.product.config.EmbeddedKafka;
+import com.jhipster.product.config.EmbeddedSQL;
+import com.jhipster.product.config.JacksonConfiguration;
+import com.jhipster.product.config.TestSecurityConfiguration;
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+import org.springframework.boot.test.context.SpringBootTest;
+
+/**
+ * Base composite annotation for integration tests.
+ */
+@Target(ElementType.TYPE)
+@Retention(RetentionPolicy.RUNTIME)
+@SpringBootTest(classes = { ProductApp.class, JacksonConfiguration.class, AsyncSyncConfiguration.class, TestSecurityConfiguration.class })
+@EmbeddedSQL
+@EmbeddedKafka
+public @interface IntegrationTest {
+    // 5s is Spring's default https://github.com/spring-projects/spring-framework/blob/main/spring-test/src/main/java/org/springframework/test/web/reactive/server/DefaultWebTestClient.java#L106
+    String DEFAULT_TIMEOUT = "PT5S";
+
+    String DEFAULT_ENTITY_TIMEOUT = "PT5S";
+}
